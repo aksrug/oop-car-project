175b025601abda59603c00bb698caafd87c750fd
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-07-21 19:46:38 +0300

    final

--- js/Car.js
+++ js/Car.js
@@ -5,68 +5,66 @@ export class Car {
         this.color = spalva;
         this.fuelTankCapacityLitres = kuroBakoTalpa;
         this.averageFuelConsumptionLitres = vidKuroSanaudos;
-        this.engineIsOn = false; 
+        this.engineIsOn = false;
         this.speed = 0;
         this.fuelLeft = kuroBakoTalpa; 
     }
 
-
     ijungtiVarikli() {
-        if(this.engineIsOn !== false) {
-            return 'Variklis yra įjungtas.'
-        } else {
+        if (this.engineIsOn) {
             return 'Įjungto variklio negalima įjungti dar kartą, sugadinsite starterį!';
-        }
+        } 
+        this.engineIsOn = true;
+        return 'Variklis yra įjungtas.';
     }
 
     isjungtiVarikli () {
-        if (this.engineIsOn === false) {
+        if (!this.engineIsOn) {
             return 'Variklis yra išjungtas, dar kartą to padaryti negalima.';
-        } else {
-            return 'Variklis yra išjungtas.'
         }
+        this.engineIsOn = false;
+        return 'Variklis yra išjungtas.';
     }
 
     pradetiVaziuoti() {
-        if (this.engineIsOn !== false) {
+        if (this.engineIsOn) {
             return 'Pradėti važiuoti negalime, kol variklis yra neįjungtas.';
-        } else {
-            this.speed = 20;
-            return `Automobilis juda ${this.speed}km/h. Kuro sąnaudos yra ${this.averageFuelConsumptionLitres * 2}l/100km.`;
-        } 
+        }
+        this.speed = 20;
+        this.fuelLeft -= this.averageFuelConsumptionLitres * 0.2; 
+        return `Automobilis juda ${this.speed}km/h. Kuro sąnaudos yra ${this.averageFuelConsumptionLitres * 2}l/100km.`;
     }
     
     vaziuoti() {
         if (this.speed > 0) {
             return `Automobilis važiuoja. Vidutinės kuro sąnaudos yra ${this.averageFuelConsumptionLitres}l/100km.`;
-        }
-        this.speed < 0;
-        return 'Automobilis stovi vietoje.';
+        } else {
+            return 'Automobilis stovi vietoje.';
+        }        
     }
 
     sustoti() {
-        if(this.speed === 0) {
+        this.speed = 0;
         return `Automobilis sustojo, nes jo greitis yra ${this.speed}km/h.`;
-        }
     }
 
     likesKuras() {
-        return `Kuro likutis bake yra ${this.fuelTankCapacityLitres - this.averageFuelConsumptionLitres}l.`        
+        return `Kuro likutis bake yra ${this.fuelLeft.toFixed(2)}l.`;      
     }
 
-    kuroPapildymas() {
-        if (this.engineIsOn === true) {
-            return `Prieš pildami kurą išjunkite automobilio variklį.`;
-        } else {
-            return `Automobilio variklis išjungtas, galime saugiai pildyti kuro baką.`;
+    kuroPapildymas(kiekis) {
+        if (this.engineIsOn) {
+            return 'Prieš pildami kurą išjunkite automobilio variklį.';
         }
-        
+        if (kiekis <= 0) {
+            return 'Kuro kiekis turi būti teigiamas skaičius.';
+        }
+        if (this.fuelLeft + kiekis > this.fuelTankCapacityLitres) {
+            let maksimalusPapildomasKiekis = (this.fuelTankCapacityLitres - this.fuelLeft).toFixed(2);
+            this.fuelLeft = this.fuelTankCapacityLitres; 
+            return `Kuro bakas perpildytas. Maksimalus papildomas kiekis: ${maksimalusPapildomasKiekis}l.`;
+        }
+        this.fuelLeft += kiekis;
+        return `Kuro bakas papildytas. Dabar kuro likutis yra ${this.fuelLeft}l.`;
     }
-
-
-
-
-    
-
-
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { Car } from "./Car.js"; 
 
-const audi = new Car ('Audi', '80', 'Geltona', 66, 5.2);
+const audi = new Car('Audi', '80', 'Geltona', 66, 5.2);
 console.log(audi);
 
 console.log(audi.name);
@@ -10,7 +10,7 @@ console.log(audi.fuelTankCapacityLitres);
 console.log(audi.averageFuelConsumptionLitres);
 console.log(audi.engineIsOn);
 console.log(audi.speed);
-//console.log(audi.fuelLeft);
+console.log(audi.fuelLeft);
 
 console.log(audi.ijungtiVarikli());
 console.log(audi.isjungtiVarikli());
@@ -18,4 +18,5 @@ console.log(audi.pradetiVaziuoti());
 console.log(audi.vaziuoti());
 console.log(audi.sustoti());
 console.log(audi.likesKuras());
-console.log(audi.kuroPapildymas());
+console.log(audi.kuroPapildymas(20));
+console.log(audi.likesKuras());
